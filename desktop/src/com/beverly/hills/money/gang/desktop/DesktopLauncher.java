352c9b9ee8c929dc8195d1d92312e889f32ca4c8
Anton Dolgopolov <131554010+beverly-hills-money-gangster@users.noreply.github.com>
2024-08-06 22:40:22 +0300

    Alt+tab support (#31)

--- desktop/src/com/beverly/hills/money/gang/desktop/DesktopLauncher.java
+++ desktop/src/com/beverly/hills/money/gang/desktop/DesktopLauncher.java
@@ -1,6 +1,8 @@
 package com.beverly.hills.money.gang.desktop;
 
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.beverly.hills.money.gang.Configs;
